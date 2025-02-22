c2cdcb2ec14be98f5877388ad54b2b0243af2bf6
Bhagawat Chapagain <112792607+bc2026@users.noreply.github.com>
2025-02-22 02:07:29 +0000

    formatting fixes in gsheets

--- db_approach/node_rest_api/gsheets.js
+++ db_approach/node_rest_api/gsheets.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
 const keys = require("./secret_key.json");
 
- async function sendDataToSheet(voter) {
+async function sendDataToSheet(voter) {
   const auth = new google.auth.GoogleAuth({
     credentials: keys,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"]
@@ -11,19 +11,31 @@ const keys = require("./secret_key.json");
   const spreadsheetId = "1J73UDJsoyfLo8puxke4agKogcFnMLM9qsfyNyGwD0o0";
   const range = "Sheet1!A2:G1000";
 
+  // Get the current data to find the last row
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: "Sheet1!A:G",
+  });
+
+  const rows = response.data.values;
+  const lastRow = rows ? rows.length + 1 : 2; // If there are rows, use the next one; else start from row 2
+
+  // Values to send
   const values = [
-        [voter.first_name,
-         voter.last_name,
-         voter.email,
-         voter.street_no,
-         voter.street_name,
-         voter.residence_city,
-         voter.residence_zip]
+    [voter.first_name, 
+     voter.last_name, 
+     voter.email, 
+     voter.phone_no, 
+     voter.street_no, 
+     voter.street_name, 
+     voter.residence_city, 
+     voter.residence_zip]
   ];
 
+  // Update the last row dynamically
   await sheets.spreadsheets.values.update({
     spreadsheetId,
-    range,
+    range: `Sheet1!A${lastRow}:G${lastRow}`,
     valueInputOption: "RAW",
     resource: { values }
   });
