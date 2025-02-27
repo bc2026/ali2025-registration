f7b57d0cda1a22a87434b1e7636690a71520d276
Bhagawat Chapagain <bchapaga@stevens.edu>
2025-02-27 11:49:19 -0500

    changed port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express(); // Initialize Express FIRST
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Serve static files (HTML, CSS, JS)
 app.get("/", (req, res) => {
