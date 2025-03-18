7dd125722ec577b137271542e7ffa35736f94a52
Bhagawat Chapagain <bchapaga@stevens.edu>
2025-03-17 20:14:59 -0400

    test with "fn"

--- dev/meta.js
+++ dev/meta.js
@@ -20,9 +20,9 @@ async function sendDataToMeta(voter) {
         const userData = new UserData()
             .setEmails([voter.email])
             .setPhones([voter.phone_no])
-            .setFirstName(voter.first_name)
-            .setLastName(voter.last_name)
-            .setZipCode(voter.registration_zip)
+            .setfn(voter.first_name)
+            // .setLastName(voter.last_name)
+            // .setZipCode(voter.registration_zip)
             .setFbp('fb.1.1558571054389.1098115397')
             .setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890');
 
