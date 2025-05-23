fc410f51aeb66b15866a3fa8bcc0b21f2cada3ee
Bhagawat Chapagain <bchapaga@stevens.edu>
2025-05-23 11:04:28 -0400

    message at bottom updat

--- multistep-form/src/Components/Footer/Footer.js
+++ multistep-form/src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = ({ step }) => {
     return (
       <footer className="bg-gray-100 py-4 text-center text-sm text-gray-600">
-        Your information is kept secure and only used to verify your voter registration status.
+        Your information is kept secure.
         <br/>
         Step {step} of 2
       </footer>
