25233483ceabd27c670851de3cebdd051df06ec8
Bhagawat Chapagain <bchapaga@stevens.edu>
2025-10-09 13:11:51 -0400

    variable name change

--- backend/gsheets.js
+++ backend/gsheets.js
@@ -1,6 +1,7 @@
 const { google } = require("googleapis");
 const keys = require("./secret_key.json");
 
+
 async function sendDataToSheet(voter, is_reg) {
   const auth = new google.auth.GoogleAuth({
     credentials: keys,
@@ -21,7 +22,8 @@ async function sendDataToSheet(voter, is_reg) {
   const lastRow = rows ? rows.length + 1 : 2; // If there are rows, use the next one; else start from row 2
 
   // Add submission date/time in ISO format or any format you prefer
-  const submission_time = new Date().toLocaleString(); // e.g., "10/9/2025, 1:00:00 PM"
+  const submission_time = new Date().toLocaleString(); 
+
   // Values to send
   const values = [
     [voter.first_name, 
@@ -31,7 +33,7 @@ async function sendDataToSheet(voter, is_reg) {
      voter.address, 
      voter.residence_zip,
      is_reg,
-     submissionDate] // Add this to your row
+     submission_time] // Add this to your row
   ];
 
   //console.log(values)
